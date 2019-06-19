0a6cf6713d004c426069c6f4087447da0668b1b9
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-06-19 16:55:41 +0300

    Capture video from user device

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,14 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+// tHE code below will capture the user video via the webcam
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+    });
+}
+
+getVideo();
